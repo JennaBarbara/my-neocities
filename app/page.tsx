2d2bf5cd11bdd771e3d175c6802a49683e6a237c
jennabarbara <jennabarbara@gmail.com>
2025-11-06 17:32:29 -0500

    add manual ripple button for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import Link from 'next/link'
 import { motion } from 'framer-motion'
 import { Bruno_Ace_SC } from 'next/font/google'
 import { getHomePath } from'@/app/home/path'
-import { useState, useCallback } from 'react'
+import { useState} from 'react'
 
 
 const bruno = Bruno_Ace_SC({
   weight: '400'
 })
+
+const delay = (ms:number) => new Promise(res => setTimeout(res, ms));
 
 const rippleClassName = "flex flex-col outline-15 outline-sky-500 size-1  rounded-full items-center justify-center"
 
@@ -34,10 +36,16 @@ const variants = {
 export default function Entrance() {
   const [hovered, setHovered] = useState<boolean>(false)
 
+  async function activateRippleManually(){
+      setHovered(true)
+      await delay(3000)
+      setHovered(false)
+    }
+  
+
 
   return (
-    // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-col min-h-screen w-full bg-linear-to-t from-sky-300 to-sky-200 items-center justify-center align  dark:bg-black">
+      <main className="flex flex-col min-h-screen w-full bg-linear-to-t from-sky-300 to-sky-200 items-center justify-center p-2 text-black">
 
           <motion.div
             className={rippleClassName}
@@ -51,22 +59,28 @@ export default function Entrance() {
               custom={1}
               animate={hovered ? "active" : "inactive"}
             >
-            <motion.div
-              className={rippleClassName}
-              variants={variants}
-              custom={2}
-              animate={hovered ? "active" : "inactive"}
-            >
-              <Link
-                className={`text-5xl z-50 outline-2 outline-black bg-white rounded-md p-4 ${bruno.className}`} 
-                href={getHomePath()} 
-                onMouseEnter={() => setHovered(true)}
-                onMouseOut={() => setHovered(false)}
-                >ENTER</Link>
-       </motion.div>
+              <motion.div
+                className={rippleClassName}
+                variants={variants}
+                custom={2}
+                animate={hovered ? "active" : "inactive"}
+              >
+                <Link
+                  className={`text-5xl z-50 outline-2 outline-black bg-white  p-4 ${bruno.className}`} 
+                  href={getHomePath()} 
+                  onMouseEnter={() => setHovered(true)}
+                  onMouseOut={() => setHovered(false)}
+                  >ENTER</Link>
+            </motion.div>
           </motion.div>
         </motion.div>
+        <div className='md:hidden absolute bottom-0 m-4'>
+          <button className='z-50 outline-2 outline-black bg-white  p-4 disabled:opacity-50' 
+           onClick={() =>activateRippleManually()}
+           disabled={hovered}
+           >activate ripple</button>
+
+        </div>
       </main>
-    // </div>
   );
 }
